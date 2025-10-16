080e856cf04658816928cf7bf9b3984ca5faa212
Nehuen Goni <nehuenemilianogoni@gmail.com>
2025-10-16 10:34:20 -0300

    feat: fix node vulnerabilities and delete .env from repo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,12 +6,12 @@ const Header = () => {
     const { cerrarSesion } = useAuth()
 
   return (
-    <header className='py-10 bg-indigo-600'>
+    <header className='py-1 bg-indigo-600 mb-10'>
         <div className='container mx-auto flex flex-col md:flex-row justify-between items-center'>
             <h1 className='font-bold text-2xl text-indigo-200 text-center'>Administrador de Pacientes de {''}
                 <span className='text-white font-black'> Veterinaria</span>
             </h1>
-            <nav className='flex flex-col items-center lg:flex-row gap-4 mt-5 lg:mt-0'>
+            <nav className='flex flex-col items-center lg:flex-row gap-5 mt-5 lg:mt-0'>
                 <Link to="/admin" className='text-white text-sm font-bold uppercase'>Pacientes</Link> 
                 <Link to="/admin/perfil" className='text-white text-sm font-bold uppercase'>Perfil</Link> 
 
